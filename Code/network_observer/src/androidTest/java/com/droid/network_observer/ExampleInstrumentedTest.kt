5238e5473b0817c53b51ce37893b0206ca5439a1
Devrath <devrath@mplgaming.com>
2022-11-17 19:47:01 +0530

    added module

--- Code/network_observer/src/androidTest/java/com/droid/network_observer/ExampleInstrumentedTest.kt
+++ Code/network_observer/src/androidTest/java/com/droid/network_observer/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.droid.android.code
+package com.droid.network_observer
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.yourcompany.android.awarenessfood", appContext.packageName)
+        assertEquals("com.droid.network_observer.test", appContext.packageName)
     }
 }
