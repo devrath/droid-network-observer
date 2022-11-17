0b3830f05903643a83bb3f88f1cc326d270cc87b
Devrath <devrath@mplgaming.com>
2022-11-17 21:36:17 +0530

    refractor

--- Code/app/src/main/java/com/droid/android/code/MainActivity.kt
+++ Code/app/src/main/java/com/droid/android/code/MainActivity.kt
@@ -1,30 +1,20 @@
 package com.droid.android.code
 
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.lifecycle.*
 import com.droid.android.code.databinding.ActivityMainBinding
 import com.droid.network_observer.NetworkObserver
 import com.droid.network_observer.NetworkState
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
-
-/**
- * Main Screen
- */
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-  private lateinit var networkMonitor: NetworkObserver
-
   private lateinit var binding: ActivityMainBinding
-  private var snackbar: Snackbar? = null
+
+  private lateinit var networkObserver: NetworkObserver
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -32,43 +22,15 @@ class MainActivity : AppCompatActivity() {
     binding = ActivityMainBinding.inflate(layoutInflater)
     setContentView(binding.root)
 
-    networkMonitor = NetworkObserver(this, lifecycle)
-    lifecycle.addObserver(networkMonitor)
+    networkObserver = NetworkObserver(this, lifecycle)
+    lifecycle.addObserver(networkObserver)
 
-    networkMonitor.networkAvailableStateFlow.asLiveData().observe(this, Observer { networkState ->
-      handleNetworkState(networkState)
+    networkObserver.networkAvailableStateFlow.asLiveData().observe(this, Observer { networkState ->
+      when (networkState) {
+        NetworkState.Unavailable -> SnackBarDisplay.showNetworkUnavailableAlert(binding.root)
+        NetworkState.Available -> SnackBarDisplay.removeNetworkUnavailableAlert()
+      }
     })
-  }
-
-  private fun showNetworkUnavailableAlert(message: Int) {
-    snackbar = Snackbar.make(binding.root, message, Snackbar.LENGTH_INDEFINITE)
-      .setActionTextColor(ContextCompat.getColor(this@MainActivity, R.color.white))
-      .setAction(R.string.retry) {
-         Toast.makeText(this@MainActivity,"Retry clicked",Toast.LENGTH_LONG).show()
-        }.apply {
-          view.setBackgroundColor(ContextCompat.getColor(this@MainActivity, R.color.colorRed))
-          show()
-        }
-  }
-
-  private fun handleNetworkState(networkState: NetworkState?) {
-    when (networkState) {
-      NetworkState.Unavailable -> showNetworkUnavailableAlert(R.string.network_is_unavailable)
-      NetworkState.Available -> removeNetworkUnavailableAlert()
-      else -> {}
-    }
-  }
-
-  private fun removeNetworkUnavailableAlert() {
-    snackbar?.let {
-      val view: View = it.view
-
-      val snackBarText = view.findViewById<View>(com.google.android.material.R.id.snackbar_text) as TextView
-      snackBarText.text = resources.getText(R.string.network_is_available)
-
-      view.setBackgroundColor(ContextCompat.getColor(this@MainActivity, R.color.colorGreen))
-    }
-    //snackbar?.dismiss()
   }
 
 }
